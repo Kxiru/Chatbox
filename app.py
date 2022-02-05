e65567cfac0b974e62de0552adf351ba19000b64
Kxiru <nkeirukaw@gmail.com>
2022-02-05 22:02:07 +0000

    Added all base files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+import database
 
 @app.route('/')
 def index():
@@ -9,6 +10,18 @@ def index():
 @app.route('/upload')
 def upload():
     return render_template('upload.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run()
