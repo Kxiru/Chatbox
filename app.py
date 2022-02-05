0c14847ee946965a1424faf451741683fac6e2ec
Kxiru <nkeirukaw@gmail.com>
2022-02-05 21:29:12 +0000

    Created base.html and extended to index.html

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def method_name():
-    return "Hello World!"
+    return render_template('static/index.html')
 
 if __name__ == "__main__":
     app.run()
