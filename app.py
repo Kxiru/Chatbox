a0ebfa08fa2ec5518feca9453e34f93ebb3dc0bb
Kxiru <nkeirukaw@gmail.com>
2022-02-05 21:48:37 +0000

    misc

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def method_name():
-    return render_template('static/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
 
 if __name__ == "__main__":
     app.run()
