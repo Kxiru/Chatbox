55b8773d57e4e4008cc90e70739b957d81a8349d
Kxiru <nkeirukaw@gmail.com>
2022-02-06 05:37:51 +0000

    Added back db file

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import database
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/upload')
 def upload():
     return render_template('upload.html')
+
 
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
 
-@app.route('/signup')
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    # if request.method == 'POST':
+
+
     return render_template('signup.html')
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run()
